ff89c6044623f460e03a578e9453d0bb5e2c4fc4
krishnakant <guptayash03996@gmail.com>
2026-02-13 18:10:45 +0530

    todays work

--- practice.js
+++ practice.js
@@ -2,3 +2,15 @@ const add = (a, b) => {
   return a + b;
 };
 console.log(add(2,3));
+const person = {
+  name: "KK",
+  normalFunc: function () {
+    console.log(this.name);
+  },
+  arrowFunc: () => {
+    console.log(this.name);
+  }
+};
+
+person.normalFunc(); // "KK"
+person.arrowFunc();  // undefined (in most cases)
