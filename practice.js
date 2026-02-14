7f2ee591f2404697e40bcde45247c82874bfe532
krishnakant <guptayash03996@gmail.com>
2026-02-14 20:19:48 +0530

    14-02-2026

--- practice.js
+++ practice.js
@@ -1,16 +1,37 @@
-const add = (a, b) => {
-  return a + b;
-};
-console.log(add(2,3));
-const person = {
-  name: "KK",
-  normalFunc: function () {
-    console.log(this.name);
-  },
-  arrowFunc: () => {
-    console.log(this.name);
-  }
-};
+// const add = (a, b) => {
+//   return a + b;
+// };
+// console.log(add(2,3));
+// const person = {
+//   name: "KK",
+//   normalFunc: function () {
+//     console.log(this.name);
+//   },
+//   arrowFunc: () => {
+//     console.log(this.name);
+//   }
+// };
 
-person.normalFunc(); // "KK"
-person.arrowFunc();  // undefined (in most cases)
+// person.normalFunc(); // "KK"
+// person.arrowFunc();  // undefined (in most cases)
+
+// Max element in array 
+const arr = [5, 2, 9, 1, 7];
+
+let max = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+  }
+}
+
+console.log(max); 
+// Min element in array
+let min =arr[0];
+for(let i=1;i<arr.length;i++){
+if(arr[i]<min){
+  min =arr[i];
+}
+}
+console.log(min);
