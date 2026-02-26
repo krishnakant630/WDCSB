afa85a273e5491521d2f7473843567d69b7be190
krishnakant <guptayash03996@gmail.com>
2026-02-27 01:19:45 +0530

    27-02-2026

--- calculator.js
+++ calculator.js
@@ -26,3 +26,12 @@ if (true) {
     var y = 20;
 }
 console.log(y);
+function singleNumber(nums) {
+    let result = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        result = result ^ nums[i];
+    }
+
+    return result;
+}
